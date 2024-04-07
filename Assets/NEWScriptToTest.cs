95ee595bbe3098e7e31abc6bc9f6ba066f42fcea
WaffleYF <51296023+WaffleAF@users.noreply.github.com>
2024-04-08 00:28:11 +0300

    This is the first file in unity project

---
+++ Assets/NEWScriptToTest.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class NEWScriptToTest : MonoBehaviour
+{
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector2.one;
+        
+    }
+    void Awake()
+    {
+        
+    }
+
+    
+    
+    // pdate is called once per frame
+    void Update()
+    {
+        
+    }
+}
